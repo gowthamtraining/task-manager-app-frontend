45d02bbea3707a81a6dbfc618dfc6cd49bee5682
pilla gowtham venkatesh <sepl.gowthamvenkatesh@gmail.com>
2023-12-30 14:43:26 +0530

    some ui changes

--- src/components/taskmanagerapp.jsx
+++ src/components/taskmanagerapp.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Button from '@mui/material/Button';
-import { ButtonGroup, Card, CardContent, TextField, Typography } from "@mui/material";
-import AddBoxIcon from '@mui/icons-material/AddBox';
+import { ButtonGroup, Card, CardContent, Checkbox, TextField, Typography } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 import CheckIcon from '@mui/icons-material/Check';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SaveIcon from '@mui/icons-material/Save';
 import CloseIcon from '@mui/icons-material/Close';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import {toast} from "react-toastify"
 import { url } from "../App";
 import CircularProgress from '@mui/material/CircularProgress';
@@ -19,11 +21,11 @@ const TaskManagerApp = () => {
     const [id,setid] = useState()
     const [isloading,setisloading] = useState(true)
     const [editable,seteditable] = useState(false)
-    console.log(url)
+    
 
     const fetchtaskdata = async () => {
         try {
-            const res = await axios.get(`${url}`,{
+            const res = await axios.get(`${url}api/task`,{
                 headers:{
                     "Content-Type":""
                 }
@@ -38,12 +40,13 @@ const TaskManagerApp = () => {
 
     useEffect(() => {
         fetchtaskdata();
+        
     }, []);
 
     const onsubmit = async () => {
 
         if(editable===true){
-            await axios.put(`${url}/${id}`,{name},{
+            await axios.put(`${url}api/task/${id}`,{name},{
                 headers:{
                     "Content-Type":"application/json"
                 }
@@ -57,7 +60,7 @@ const TaskManagerApp = () => {
         }
         else{
         try {
-            await axios.post(`${url}`, { name }, {
+            await axios.post(`${url}api/task`, { name }, {
                 headers: {
                     "Content-Type": "application/json"
                 }
@@ -71,7 +74,7 @@ const TaskManagerApp = () => {
     }
     };
     const DeleteTaskById =(id)=>{
-        axios.delete(`${url}/${id}`,{
+        axios.delete(`${url}api/task/${id}`,{
             headers:{
                 "Content-Type":""
             }
@@ -92,9 +95,9 @@ const TaskManagerApp = () => {
         })
     }
     const selecteditem=(id)=>{
-        axios.get(`${url}/${id}`).then((res)=>{
+        axios.get(`${url}api/task/${id}`).then((res)=>{
             if(res){
-               axios.put(`${url}/${id}`,{name:res.data.name,complete:!res.data.complete}).then((res)=>{
+               axios.put(`${url}api/task/${id}`,{name:res.data.name,complete:!res.data.complete}).then((res)=>{
                 fetchtaskdata()
                 if(res.data.complete){
                 toast.success("data selected")
@@ -129,35 +132,45 @@ const TaskManagerApp = () => {
                                       fullWidth
                                   />
                                   <Button
-                                      variant="outlined"
-                                      startIcon={<AddBoxIcon />}
+                                      variant="contained"
+                                    style={{borderRadius: '50%',
+                                    minWidth: '50px',
+                                    height: '40px',
+                                    padding: 0,
+                                    boxShadow: "0 0 10px blue)"
+                            
+
+
+                                    
+                                }}
+                                      
                                       onClick={() => onsubmit()}
                                   >
-                                     {editable?"edit":"Add"}
+                                    <AddIcon/>
                                   </Button>
                               </div>
                               <div style={{ display: "flex", justifyContent: "space-between" }}>
                                   <Typography style={{fontSize:"14px"}}>No Of Tasks: {taskkdata.length}</Typography>
                                   <Typography style={{fontSize:"14px"}}>No of Tasks Completed:{taskkdata.filter((res)=>res.complete === true).length}</Typography>
                               </div>
-                              <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+                              <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
                                   {taskkdata.map((item) => (
                                       <div
                                           key={item._id}
                                           style={{
                                               display: "flex",
                                               justifyContent: "space-between",
-                                              border: "1px solid black",
+                                              
                                               padding: "5px 7px",
                                               borderRadius: "5px",
+                                              boxShadow: item.complete ? "0 0 10px rgba(0, 0, 0, 0.1)" : "none"
+
                                           }}
+                                          
                                       >
+                                        <Checkbox icon={<RadioButtonUncheckedIcon />} checkedIcon={<RadioButtonCheckedIcon />} checked={item.complete}   onClick={()=>selecteditem(item._id)}/>
                                           <Typography>{item.name}</Typography>
                                           <ButtonGroup size="small">
-                                              <Button size="small">
-                                                  {item.complete?
-                                                  <CheckIcon fontSize="small" onClick={()=>selecteditem(item._id)} />:<CloseIcon fontSize="small" onClick={()=>selecteditem(item._id)}/>}
-                                              </Button>
                                               <Button size="small" onClick={()=>DeleteTaskById(item._id)}>
                                                   <DeleteIcon fontSize="small" />
                                               </Button>
